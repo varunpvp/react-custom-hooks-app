78ab783295c0399171e75442847d015f1b49b379
Varun Pujari <varunpvp@gmail.com>
2020-04-21 13:59:25 +0530

    setup gp-pages

--- src/hooks/languagesHook.ts
+++ src/hooks/languagesHook.ts
@@ -4,8 +4,11 @@ import { useState, useEffect } from "react";
 const useLanguages = () => {
   const [languages, setLanguages] = useState<Language[]>([]);
 
+  const endpoint =
+    "https://gist.githubusercontent.com/varunpvp/74e4752d88f3fa8b21fc671b7eda04f4/raw/208e964bc87f671f81d3c19efd855fb8905cd32d/programming-languages.json";
+
   useEffect(() => {
-    fetch("/languages.json")
+    fetch(endpoint)
       .then((response) => response.json())
       .then((data) => setLanguages(data))
       .catch((error) => console.log("catch useLanguages", { error }));
