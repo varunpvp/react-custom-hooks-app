830b5e52dda3ed582e2970883b1456fc728a7815
Varun Pujari <varunpvp@gmail.com>
2020-04-21 12:45:58 +0530

    refactor

--- src/hooks/userInputHook.ts
+++ src/hooks/userInputHook.ts
@@ -3,13 +3,11 @@ import { useState, useCallback } from "react";
 const useUserInput = (defaultValue: string = "") => {
   const [value, setValue] = useState(defaultValue);
 
-  const onChange = useCallback(e => {
-    setValue(e.target.value);
-  }, []);
+  const onChange = useCallback((e) => setValue(e.target.value), []);
 
   return {
     value,
-    onChange
+    onChange,
   };
 };
 
